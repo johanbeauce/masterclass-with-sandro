43e6d663205d05886d83b84db7e92a093d3207da
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-09-18 15:14:44 +0200

    Add project structure from Spring initializr and Junit 5 dependencies

---
+++ src/main/java/be/beauce/training/masterclasswithsandro/MasterclassWithSandroApplication.java
@@ -0,0 +1,13 @@
+package be.beauce.training.masterclasswithsandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterclassWithSandroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterclassWithSandroApplication.class, args);
+    }
+
+}
